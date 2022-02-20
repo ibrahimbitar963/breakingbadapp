07538760ac2627f895e48dbc58f4c7a230fb2099
ibrahimbitar963 <ibrahim.bitar963@gmail.com>
2022-02-20 20:27:09 +0200

    cubit2

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:breakingbad/constans/strings.dart';
-import 'package:breakingbad/presentation/widgets/screen/characters_details.dart';
+import 'package:breakingbad/presentation/screen/characters_details.dart';
+import 'package:breakingbad/presentation/screen/characters_screen.dart';
 import 'package:flutter/material.dart';
-import 'presentation/widgets/screen/characters_screen.dart';
 class AppRouter{
   Route? generateRoute(RouteSettings settings){
     switch(settings.name){
--- lib/business_logic/cubit/charachters_cubit.dart
+++ lib/business_logic/cubit/charachters_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:breakingbad/data/model/character.dart';
+import 'package:breakingbad/data/repository/characters_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'charachters_state.dart';
 
 class CharachtersCubit extends Cubit<CharachtersState> {
-  CharachtersCubit() : super(CharachtersInitial());
+  final CharacterRepo characterRepo;
+  late List<Character> characters;
+  CharachtersCubit(this.characterRepo) : super(CharachtersInitial());
+  List<Character> getAllCharacter(){
+    characterRepo.getAllCharacters().then((characters) {
+      emit(CharachtersLoaded(characters));
+            this.characters = characters;
+    });
+    return characters;
+  }
 }
